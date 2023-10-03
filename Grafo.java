67c6fe8ab5e5df405f9a671bdc9127389faaacd0
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2023-10-02 22:06:07 -0300

    Create Grafo.java

---
+++ Grafo.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Grafo {
+    private int numVertices;
+    private int[][] matrizAdjacencia;
+
+    public Grafo(int numVertices) {
+        this.numVertices = numVertices;
+        matrizAdjacencia = new int[numVertices][numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            Arrays.fill(matrizAdjacencia[i], 0);
+        }
+    }
+
+    public void adicionarAresta(int origem, int destino, int peso) {
+        matrizAdjacencia[origem][destino] = peso;
+        // Se o grafo for não direcionado, adicione também a aresta de destino para origem
+        matrizAdjacencia[destino][origem] = peso;
+    }
+
+    public void mostrarMatrizAdjacencia() {
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                System.out.print(matrizAdjacencia[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
