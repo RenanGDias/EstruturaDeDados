8e99e15275eb10c72f5e2baf580626900aaed466
Renan Dias <129848338+RenanGDias@users.noreply.github.com>
2023-10-02 23:14:07 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Grafo grafo = new Grafo(5);
+        Grafo grafoLista = new Grafo(5);
 
-        grafo.adicionarAresta(0, 1, 1);
-        grafo.adicionarAresta(0, 2, 1);
-        grafo.adicionarAresta(1, 3, 1);
-        grafo.adicionarAresta(2, 4, 1);
+        grafoLista.adicionarArestaLista(0, 1);
+        grafoLista.adicionarArestaLista(0, 2);
+        grafoLista.adicionarArestaLista(1, 3);
+        grafoLista.adicionarArestaLista(2, 4);
 
-        System.out.println("Matriz de Adjacência do Grafo:");
-        grafo.mostrarMatrizAdjacencia();
+        Grafo grafoMatriz = new Grafo(5);
+
+        grafoMatriz.adicionarArestaMatriz(0, 1, 1);
+        grafoMatriz.adicionarArestaMatriz(0, 2, 1);
+        grafoMatriz.adicionarArestaMatriz(1, 3, 1);
+        grafoMatriz.adicionarArestaMatriz(2, 4, 1);
+
+        System.out.println("Lista de Adjacência do Grafo:");
+        grafoLista.mostrarListaAdjacencia();
+        System.out.println("\n\nMatriz de Adjacência do Grafo:");
+        grafoMatriz.mostrarMatrizAdjacencia();
     }
 }
